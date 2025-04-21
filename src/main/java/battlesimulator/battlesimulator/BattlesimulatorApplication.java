239b982557bdea4f736550aaffd01c327a354137
devaajanne <devaajanne@gmail.com>
2025-04-21 18:23:54 +0300

    Initial commit

---
+++ src/main/java/battlesimulator/battlesimulator/BattlesimulatorApplication.java
@@ -0,0 +1,13 @@
+package battlesimulator.battlesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlesimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlesimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/battlesimulator/battlesimulator/BattlesimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package battlesimulator.battlesimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlesimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
